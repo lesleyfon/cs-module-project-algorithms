4ba606018c005eaa3f893ae1f76d7aac2b3083e4
Lesley Banadzem <lesleybanadzem2017@gmail.com>
2020-07-14 12:27:46 -0700

    completed none zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,28 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
-
+    '''
+        Naive Solution:
+            1 create a store to hold none 0 ints and 0 digits
+            2. loop though it and remove every element from the array and add to their respective arrays
+            3. Concat the zero array to the none zero array. 
+            5. Concat every thing in the none_zero array into the arr and return it 
+    '''
+    zero_store = []
+    none_zero = []
+    while len(arr) > 0:
+        if arr[0] == 0:
+            zero_store.append(arr.pop(0))
+        else:
+            none_zero.append(arr.pop(0))
     pass
+    none_zero.extend(zero_store)
+    arr.extend(none_zero)
+    return arr
 
 
 if __name__ == '__main__':
