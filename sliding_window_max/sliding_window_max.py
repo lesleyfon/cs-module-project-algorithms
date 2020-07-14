a073ecc2daa03fdda58702794bea93f453812358
Lesley Banadzem <lesleybanadzem2017@gmail.com>
2020-07-14 13:20:00 -0700

    added pseudocode for the slinding_window_max'

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -6,7 +6,15 @@ Returns: a List of integers
 
 def sliding_window_max(nums, k):
     # Your code here
-
+    '''
+        Steps:
+            1 - Create a list to hold all the max in a given window.
+            2 - Create two variables to keep track of the begining index of a window and the end index of a window. 
+            3 - While the end index of a window is less than the length of the length of the nums list. we want to keep looping
+            4 - Create a variable to hold the elements in a current window and slice those elements using the start_window_index and end_window_index
+            5 - Get the max value in the current_window and add it to the window_max list
+            6 - Return the window_max list at the end of the loop
+    '''
     window_max = []
     start_window_index = 0
     end_window_index = k - 1
